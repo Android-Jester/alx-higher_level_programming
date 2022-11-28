25fa8958a33e9ece854c9dddec2c9a2299f22cf1
Kwadwo Adjei Duah <duah14@outlook.com>
2022-11-28 19:31:31 +0000

    python complete

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,11 +1,9 @@
+#include "lists.h"
 /**
  * int check_cycle(listint_t *list) - checks the linkedlist if there are any cycles
  *
  * Return: 1 if there are cycles and 0 if there are none.
  */
-#include "lists.h"
-
-
 int check_cycle(listint_t *list) {
   listint_t *initial;
   listint_t *current;
@@ -14,7 +12,7 @@ int check_cycle(listint_t *list) {
   current = initial;
   while (current != NULL)
   {
-    if (current->n == initial->n) {
+    if (current->next == initial->next) {
       return 1;
     }
     current = current->next;
