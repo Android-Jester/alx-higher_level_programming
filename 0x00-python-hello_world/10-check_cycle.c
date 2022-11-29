cef7390aee66278ed836649ba94f534ebcd396ca
Kwadwo Adjei Duah <duah14@outlook.com>
2022-11-29 02:28:55 +0000

    python complete

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,40 @@
 #include "lists.h"
 /**
- * int check_cycle(listint_t *list) - checks the linkedlist if there are any cycles
+ * int check_cycle(listint_t *list) - checks the linkedlist if there are any
+ * cycles
  *
  * Return: 1 if there are cycles and 0 if there are none.
  */
-int check_cycle(listint_t *list) {
-  listint_t *initial;
-  listint_t *current;
+int check_cycle(listint_t *list)
+{
+	listint_t *next;
+	listint_t *prev;
 
-  initial = list;
-  current = initial;
-  while  (initial && initial->next && current && current->next)
-  {
-    if (current->n == initial->n) {
-      return 1;
-    }
-    current = current->next;
-  }
-  return 0;
+	next = list;
+	prev = list;
+	while (list && next && next->next)
+	{
+		list = list->next;
+		next = list->next;
+		if (list == next)
+		{
+			list = prev;
+			prev =  next;
+			while (1)
+			{
+				next = prev;
+				while (next->next != list && next->next != prev)
+				{
+					next = next->next;
+				}
+				if (next->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
+	}
+
+	return (0);
 }
-
-
--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+import this
 number = 98
 print(f"{number:d} Battery Street\n")
+
+
