c3c0f42ed1c3d09f14229db972ce13df0bc72f3a
Kwadwo Adjei Duah <duah14@outlook.com>
2022-11-29 22:10:48 +0000

    finally

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i < j:
-            print(f"{i:d}{j:d}", end="")
+            print("{}{}".format(i, j), end="")
             if i < 8:
                 print(", ", end="")
