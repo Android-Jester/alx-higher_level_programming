a77dfd5e1f728d646a03e098a061d9eb811d53a7
Kwadwo Adjei Duah <duah14@outlook.com>
2022-11-29 02:33:01 +0000

    python complete

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,36 +5,19 @@
  *
  * Return: 1 if there are cycles and 0 if there are none.
  */
+
+
+
 int check_cycle(listint_t *list)
 {
-	listint_t *next;
-	listint_t *prev;
-
-	next = list;
-	prev = list;
-	while (list && next && next->next)
-	{
-		list = list->next;
-		next = list->next;
-		if (list == next)
-		{
-			list = prev;
-			prev =  next;
-			while (1)
-			{
-				next = prev;
-				while (next->next != list && next->next != prev)
-				{
-					next = next->next;
-				}
-				if (next->next == list)
-					break;
-
-				list = list->next;
-			}
+	listint_t *current;
+	listint_t *initial;
+	initial = list;
+	while(initial && current && current->next) {
+		if(current->n == initial->n) {
 			return (1);
 		}
+		current = current->next;
 	}
-
 	return (0);
 }
