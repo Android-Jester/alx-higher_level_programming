9b28903da0f2db82afdff9e414d5a609753a5171
Kwadwo Adjei Duah <duah14@outlook.com>
2022-12-02 01:24:57 +0000

    finally

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv, argc, val = sys.argv[1:], len(sys.argv[1:]), 0
+    argv = sys.argv[1:]
+    argc = len(sys.argv[1:])
+    val = 0
     for i in range(argc):
         val += int(sys.argv[i])
     print(f"{val:d}")
