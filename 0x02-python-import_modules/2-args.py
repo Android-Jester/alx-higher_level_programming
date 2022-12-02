b4e25e33419ea216fa7398848fea8c128abda6a0
Kwadwo Adjei Duah <duah14@outlook.com>
2022-12-02 01:33:21 +0000

    finally

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
-    argc = len(argv) + 1
+    argc = len(argv)
     if argc == 0:
         print(f"{argc:d} arguments.")
     elif argc == 1:
@@ -10,5 +10,5 @@ if __name__ == "__main__":
         print(f"1: {sys.argv[1]}")
     else:
         print(f"{argc:d} arguments:")
-    for i in range(argc):
+    for i in range(argc + 1):
         print(f"{i+1:d}: {sys.argv[i]:s}")
