313d83309e7d0e09a718bac297bd9795e53cc194
Kwadwo Adjei Duah <duah14@outlook.com>
2022-12-02 01:29:00 +0000

    finally

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     argc = len(sys.argv[1:])
     val = 0
-    for i in range(argc):
+    for i in range(argc + 1):
         val += int(sys.argv[i])
     print(f"{val:d}")
     
